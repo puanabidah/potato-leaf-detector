2508b86e1cc1739fdb416dccae1dc6dc2b3363b0
Puan Abidah Nitisara <puanabidah13@gmail.com>
2023-12-11 17:38:43 +0700

    first commit

---
+++ api/main.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from io import BytesIO
+from PIL import Image
+import numpy as np
+import tensorflow as tf
+import uvicorn
+import h5py
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+MODEL = tf.keras.models.load_model(h5py.File('saved_model/potato-leaf.h5', 'r'))
+CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+
+@app.get("/ping")
+async def ping():
+    return "Hello, Im alive!"
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+@app.post("/predict")
+async def predict(
+    file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+
+    predictions = MODEL.predict(img_batch)
+
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8080)
